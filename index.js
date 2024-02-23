d582df4deb559fb6e87a021f3d4470f0872e75be
Holberton iMac 17 <holbertonimac17@Holbertons-iMac.local>
2024-02-23 10:19:05 -0500

    simplify the code

--- index.js
+++ index.js
@@ -13,8 +13,53 @@ page.setDefaultNavigationTimeout(0);
 // URL page to scrap data
 const mainPage = 'https://www.gsmarena.com/';
 
+/*
+async function extractData (pathPage, selector, pathData, details = false) {
+    // Tell the tab to navigate to the page.
+    await page.goto(pathPage);
+    // Go to the selector
+    page.waitForSelector(selector);
+
+    return await page.$$eval(pathData, (info) => {
+        return info.map($data => {
+            const $link = $data.querySelector("a");
+    
+            const toText = (element) => element && element.innerText.trim();
+            const getPath = (element) => element && element.getAttribute('href').trim();
+            if (details === true) {
+             const toDetails = (element) => element && element.title.trim();
+             return {
+                details: toDetails($link),
+                path: getPath($link), 
+             }
+            }
+
+            return {
+                name: toText($link),
+                path: getPath($link),
+            };
+        });
+    });     
+}
+
+const brands = extractData(mainPage, "#body > aside > div.brandmenu-v2.light.l-box.clearfix", '#body > aside > div.brandmenu-v2.light.l-box.clearfix > ul > li');
+*/
+/*
+const data = [];
+
+for (const key of brands) {
+    const { path, name } = key;
+    const references = extractData(`${mainPage}/${path}`, "#review-body > div", '#review-body > div > ul > li', true);
+    data.push({
+        celulares: references,
+        marca: name,
+    });
+}
+*/
+
 // Tell the tab to navigate to the mobile phone page.
 await page.goto(mainPage);
+
 
 // capture of brand and the path
 page.waitForSelector("#body > aside > div.brandmenu-v2.light.l-box.clearfix");
@@ -33,7 +78,7 @@ const brands = await page.$$eval('#body > aside > div.brandmenu-v2.light.l-box.c
     });
 });
 
-//console.log(brands);
+/*
 // test to get the phat from de variable "brand"
 
 // capture of brand references and path
@@ -64,7 +109,8 @@ for (const key of brands) {
             marca: name,
         });
 }
-
-console.log(data)
+*/
+//console.log(data)
 // Turn off the browser to clean up after ourselves.
+console.log(brands);
 await browser.close();
